0c81e45102bce28060e85649800705e0529d35e9
Sandeep Sharma <sandeeprolex69@gmail.com>
2025-06-06 09:00:24 +0000

    Operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,8 +1,8 @@
-let score = "Sandeep"
+// let score = "Sandeep"
 // console.log(typeof score);
 // console.log(typeof (score));
 
-let valueInNumber = Number(score) 
+// let valueInNumber = Number(score) 
 // console.log(typeof valueInNumber);
 // console.log( valueInNumber);
 
@@ -12,16 +12,49 @@ let valueInNumber = Number(score)
 
 // let isLoggedIn = 1
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
 // console.log(booleanIsLoggedIn);
 
 // 1 => true , 0 => false
 // "" => false
 // "Sandeep" => true
 
-let someNumber = 33
-let stringNumber = String(someNumber)
-console.log( typeof stringNumber);
+// let someNumber = 33
+// let stringNumber = String(someNumber)
+// console.log( typeof stringNumber);
+
+//********************Operations*********************
+
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/4);
+// console.log(2%3);
+
+let str1 = "hii"
+let str2 = "Sandeep"
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log("1" + "1");
+// console.log(1 + 2 + 2);
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log("1" + "2" +"2");
+
+console.log(true);
+console.log(+true);
+console.log(+"");
+
+
 
 
 
